5b37908b78429374cad6918448849e3265322b50
Krishna kumar <2030049@kiit.ac.in>
2023-06-08 13:20:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// we want the hamburger menu clickble when we click we want navbar to open
+const bar = document.getElementById('bar') // variable name is bar
+const close = document.getElementById('close')  
+const nav = document.getElementById('navbar') // by clicking on hamburger we are opening naavbar
+
+if(bar) {   // if bar is already open then will check
+    bar.addEventListener('click',() => { // if click
+        nav.classList.add('active');  // open hamburger
+    }) 
+}
+if(close) {
+    close.addEventListener('click',() => 
+    { // if anyone click on close then close not add this time 
+        nav.classList.remove('active');
+})}
